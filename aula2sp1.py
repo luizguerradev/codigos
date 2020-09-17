35c2525abf64810901ddc66a9bd2a2544ca4f1d0
Luiz Guerra <luizhc.guerra@gmail.com>
2020-09-17 10:35:20 -0300

    aprendizados em aula

---
+++ aula2sp1.py
@@ -0,0 +1,16 @@
+def ValorLista():
+    for i in range(1,5):
+        lista.append(int(input(f'Digite o valor da sua prova {i}:')))
+
+def Media():
+    s=0
+    for i in range(len(lista)):
+        s = s+lista[i]
+    m= s /4
+    return m
+
+
+lista = []
+ValorLista ()
+m= Media()
+print (m)
---
+++ aula2sp2.py
@@ -0,0 +1,12 @@
+#Busca sequencial#
+#Percorre todos os elementos#
+
+def BuscaSeq(list, item):
+    pos =0
+    x = False
+
+    while pos<len(list) and not
+
+lista = [5, 6, 8, 12, 1, 5, 7]
+print (BuscaSeq(lista, 8))
+print (BuscaSeq(lista, 13))
